abd0807eb8e06b85f59976553de91509e2139fb7
Marius998 <marius.hammer@smail.th-koeln.de>
2021-11-24 20:55:33 +0100

    create payload for from-to

--- custom_nodes/from-to.js
+++ custom_nodes/from-to.js
@@ -2,8 +2,11 @@ module.exports = function(RED) {
     function FromTo(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+        var from = config.from;
+        var to = config.to;
+        console.log(from,to);
         node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
+            msg.payload = {"type":"","ts":"","from":from,"to":to}
             node.send(msg);
         });
     }
