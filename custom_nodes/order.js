c7da08fb2e9b3f00781ac2cd5331600f5e543606
Marius998 <marius.hammer@smail.th-koeln.de>
2021-11-24 20:59:09 +0100

    edit input values and add them to payload

--- custom_nodes/order.js
+++ custom_nodes/order.js
@@ -2,11 +2,13 @@ module.exports = function(RED) {
     function Order(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+        var quality = config.quality;
+        var color = config.color;
         node.on('input', function(msg) {
             msg.payload = {
                 "workpiece": {
-                    "type": "RED",
-                    "state": "RAW"
+                    "type": color,
+                    "state": quality
                 },
                 "ts": "",
                 "code": 3
