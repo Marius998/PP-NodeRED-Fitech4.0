ac72cd5af06d5e888c963bd4371062fcf0b7cf1d
Marius998 <marius.hammer@smail.th-koeln.de>
2021-11-25 10:02:49 +0100

    add mqtt client test - not working

--- custom_nodes/order.js
+++ custom_nodes/order.js
@@ -1,3 +1,4 @@
+const mqtt = require('mqtt');
 module.exports = function(RED) {
     function Order(config) {
         RED.nodes.createNode(this,config);
@@ -15,6 +16,31 @@ module.exports = function(RED) {
             }
             node.send(msg);
         });
+
+        /*Custom MQTT CLient*/
+        let options = {
+            username: config.username,
+            password: config.password,
+            connectTimeout: 7 * 1000
+        };
+
+        let client = mqtt.connect(`mqtt://${config.host}:${config.port}`, options);
+        console.log("mqtt client connecting ... ")
+        console.log(options);
+        client.on('connect', function () {
+            console.log("Client Connected")
+            client.subscribe('f/i/state/hbw', function (err) {
+                if (!err) {
+                    client.publish('presence', 'Hello mqtt')
+                }
+            })
+        })
+        client.on('message', function (topic, message) {
+            // message is Buffer
+            console.log(message.toString())
+            client.end()
+        })
+        /*Custom MQTT Client net*/
     }
     RED.nodes.registerType("order",Order);
 }
